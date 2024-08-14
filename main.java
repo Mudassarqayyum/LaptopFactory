a34b0447cf68790b7568200902de3b19bf5356f2
Mudassar Qayyum <74399438+Mudassarqayyum@users.noreply.github.com>
2024-08-14 19:02:02 +0100

    Update main.java
    added abstrat class and method

--- main.java
+++ main.java
@@ -1,23 +1,15 @@
-public class Laptop {
+abstract class Laptop {
     private String brand;
     private String model;
     private String color;
-    private int ram;
-    private int cpu;
+    private String ram;
+    private String cpu;
     private String Storage;
     private double price;
     private String graphicCard;
 
-    public Laptop(String brand, String model, String color, int ram, int cpu, String Storage, double price, String graphicCard) {
-        this.brand = brand;
-        this.model = model;
-        this.color = color;
-        this.ram = ram;
-        this.cpu = cpu;
-        this.Storage = Storage;
-        this.price = price;
-        this.graphicCard = graphicCard;
-    }
+
+    abstract   void buildLaptop();
     public void DisplayLaptopDetails() {
         System.out.println("Brand: " + brand);
         System.out.println("Model: " + model);
@@ -29,23 +21,47 @@ public class Laptop {
         System.out.println("GraphicCard: " + graphicCard);
 
     }
-public class OfficeLaptops extends Laptop {
-    public OfficeLaptops(String brand, String model, String color, int ram, int cpu, String Storage, double price, String graphicCard) {
-            super(brand, model, color, ram, cpu, Storage, price, graphicCard);
-        }
 }
-public class GamingLaptops extends Laptop {
-    public GamingLaptops(String brand, String model, String color, int ram, int cpu, String Storage, double price, String graphicCard) {
-            super(brand, model, color, ram, cpu, Storage, price, graphicCard);
-        }
+class OfficeLaptops extends Laptop {
+    public OfficeLaptops(String brand, String model, String color, String ram, String cpu, String Storage, double price, String graphicCard) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.ram = ram;
+        this.cpu = cpu;
+        this.Storage = Storage;
+        this.price = price;
+        this.graphicCard = graphicCard;
     }
 
-public  LaptopFactory{
-        public s
+    @Override
+    void buildLaptop() {
+        System.out.println("Build Office Laptop");
+}
+class GamingLaptops extends Laptop {
+    public GamingLaptops(String brand, String model, String color, String ram, String cpu, String Storage, double price, String graphicCard) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.ram = ram;
+        this.cpu = cpu;
+        this.Storage = Storage;
+        this.price = price;
+        this.graphicCard = graphicCard;
+    }
+
+    @Override
+    void buildLaptop() {
+        System.out.println("Build Gaming Laptop");
+    }
 }
 
-}
-    public class Main {
-    }
-    public static void main(String[] args) {
-}
+
+//
+//    public class Main {
+//    public static void main(String[] args) {
+//        OfficeLaptops officeLaptops = new OfficeLaptops("Dell","i50","black","35 gb","dell 5110","ssd 2 TB",360,"intel 54T 8GB") {
+//
+//        };
+//}
+//    }
